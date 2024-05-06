754ede7ea4af5f225f8e0e69198ac9f9621954f2
henrique <139483039+henrique140372@users.noreply.github.com>
2024-05-06 18:45:07 -0300

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
+        port: 10000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: '*',
         livereload: 35729
